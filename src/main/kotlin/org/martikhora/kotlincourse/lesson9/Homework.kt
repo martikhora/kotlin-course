a4647292f11b23e6372f38b9e09ff12614c40af2
martikhora <martikhora@yandex.ru>
2024-10-06 18:44:09 +0300

    intial commit

---
+++ src/main/kotlin/org/martikhora/kotlincourse/lesson9/Homework.kt
@@ -0,0 +1,17 @@
+package org.martikhora.kotlincourse.lesson9
+
+class Homework {
+
+    fun happyBirthdaySasha() {
+
+        print("Введите имя: ")
+        val name = readLine()
+
+        if (name == "Саша") {
+            println("Привет, Саша!")
+            println("Я немного опоздала с этим поздравлением")
+            println("Но вот оно!")
+            println("Поздравляю тебя с Днем рождения! Будь счастлив и пусть исполнятся все мечты!")
+        }
+    }
+}
